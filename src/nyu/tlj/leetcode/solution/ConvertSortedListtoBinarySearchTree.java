5ee4015281e91fa370fce0b271b1318d2bd92fc8
hugh6876 <hugh6876@gmail.com>
2014-09-23 21:36:08 -0400

    first add

---
+++ src/nyu/tlj/leetcode/solution/ConvertSortedListtoBinarySearchTree.java
@@ -0,0 +1,62 @@
+package nyu.tlj.leetcode.solution;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import nyu.tlj.leetcode.common.ListNode;
+
+public class ConvertSortedListtoBinarySearchTree {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public TreeNode sortedListToBST(ListNode head) {
+		if( head == null){
+			return null;
+		}
+		int[] array = null;
+		array = getArrayFromList(head);
+		
+		return sortedListToBST(array,0,array.length-1);
+	}
+	
+	private TreeNode sortedListToBST(int[] num,int start,int end) {
+		TreeNode node = new TreeNode(0);
+		if( start > end ){
+			return null;
+		}
+		int mid = (start + end)/2;
+		node.val = num[mid];
+		
+		node.left = sortedListToBST(num,start,mid-1);
+		node.right = sortedListToBST(num,mid+1,end);
+		
+		return node;
+		
+	}
+	
+	
+	private int[] getArrayFromList(ListNode head) {
+		ListNode tmpHead = head;
+		int len = 0;
+		while(tmpHead != null){
+			len++;
+			tmpHead = tmpHead.next;
+			
+		}
+		int[] array = new int[len];
+		
+		tmpHead = head;
+		len = 0;
+		while(tmpHead != null){
+			array[len++] = tmpHead.val;
+			tmpHead = tmpHead.next;
+			
+		}
+		
+		return  array;
+	}
+
+}
