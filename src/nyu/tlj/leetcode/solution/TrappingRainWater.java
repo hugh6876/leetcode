a84f5e2cda2a9e1414002ebdfcf740be4d355e9b
hugh6876 <hugh6876@gmail.com>
2014-10-07 13:34:29 -0400

    add

---
+++ src/nyu/tlj/leetcode/solution/TrappingRainWater.java
@@ -0,0 +1,10 @@
+package nyu.tlj.leetcode.solution;
+//http://www.meetqun.com/forum.php?mod=viewthread&tid=788
+public class TrappingRainWater {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
