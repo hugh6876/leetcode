02d0fb813e8e6e5e4d4165e99688dc491b66a672
hugh6876 <hugh6876@gmail.com>
2015-01-10 20:40:20 -0500

    first add

---
+++ src/nyu/tlj/leetcode/solution/AddBinary.java
@@ -0,0 +1,42 @@
+package nyu.tlj.leetcode.solution;
+
+public class AddBinary {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String a = "11";
+		String b = "11",res;
+		
+		AddBinary ada = new AddBinary();
+		res =  ada.addBinary(a, b);
+		
+		
+		System.out.println(res);
+
+	}
+
+	public String addBinary(String a, String b) {
+		StringBuilder res = new StringBuilder();
+		int lena = a.length() - 1, lenb = b.length() - 1;
+		int carry = 0,sum, numa, numb;
+		while(lena >= 0 || lenb >= 0 ){
+			sum = 0;
+			numa = lena < 0 ? 0 : Integer.valueOf( a.charAt(lena--) )  - 48;
+			numb = lenb < 0 ? 0 : Integer.valueOf( b.charAt(lenb--) )  - 48;
+			sum = numa + numb + carry;
+			System.out.println(sum);
+			carry = 0;
+			if( sum >= 2 ) {
+				carry = 1;
+				sum = sum - 2;
+			}
+			res.append(sum);
+		}
+		if( carry == 1 ) {
+			res.append(1);
+		}
+		
+		return res.reverse().toString();
+	}
+
+}
